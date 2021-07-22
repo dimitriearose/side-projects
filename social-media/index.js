8a23dff04e89cec9bb78efaab6d55c2c0577a09c
dimitriearose <dimitri.ea.rose@gmail.com>
2021-07-22 15:21:35 -0400

    test

--- social-media/index.js
+++ social-media/index.js
@@ -1,49 +1,19 @@
-const {ApolloServer} = require('apollo-server')
-const gql = require('graphql-tag')
-const mongoose = require('mongoose')
-const {MONGODB} =require('./config')
-const Post = require('./models/Post')
-const User = require('./models/User')
+const { ApolloServer } = require("apollo-server")
+const mongoose = require("mongoose")
+const { MONGODB } = require("./config")
+const typeDefs = require("./graphql/typedefs")
+const resolvers = require("./graphql/resolvers")
 
-const typeDefs = gql`
-    type Post{
-        id:ID!
-        body:String!
-        createdAt:String!
-        username:String! 
-    }
-    type Query{
-        getPosts: [Post]
-    }
-`
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
 
-const resolvers = {
-    Query: {
-        async getPosts(){
-            try {
-                const posts = await Post.find()
-                return posts
-            } catch (error) {
-                throw new Error(error)
-            }
-        }
-    }
- }
-
- const server = new ApolloServer({
-     typeDefs,
-     resolvers
- })
-
- mongoose
-    .connect(MONGODB, {useNewUrlParser:true, useUnifiedTopology:true})
-    .then(() => {
-        console.log('mongo')
-        return server.listen({port: 5000})
-        .then(res => {
-            console.log(`server running on port ${res.url}`)
-        })
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("mongo")
+    return server.listen({ port: 5000 }).then((res) => {
+      console.log(`server running on port ${res.url}`)
     })
-
-
-    
+  })
